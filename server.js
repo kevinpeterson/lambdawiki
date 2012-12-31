1746031fdab42f49355d710f8885af80271aabb1
Kevin Peterson <kevin.peterson@mayo.edu>
2012-12-31 14:32:36 -0600

    Only pg.

--- server.js
+++ server.js
@@ -72,17 +72,18 @@ try {
 			port: rtg.port,
 			auth: rtg.auth
 		};
-	} else if (process.env.DATABASE_URL) {
+	}// else if (process.env.DATABASE_URL) {
 		require('pg');
 		options.db = {
 			uri: process.env.DATABASE_URL,
 			type: 'pg',
 			create_tables_automatically: true
 		};
+		/*
 	} else {
 		require('redis');
 		options.db = {type: 'redis'};
-	}
+	}*/
 } catch (e) {
 	console.log(e);
 }
